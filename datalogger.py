f9f2c736cd24306ebe6aa1bb4c881ab6638ffc66
drush <rush.daniel95@gmail.com>
2020-11-04 21:34:03 +0000

    some fixes to the display class that allows for better use of screen and checks of out of bounds character writing

--- datalogger.py
+++ datalogger.py
@@ -16,7 +16,9 @@ def main():
   th = temp_humid.TempHumid(i2c)
 
   while True:
-    oled.Write(0, "T:%dF H:%d P:%d"%(hpt.GetTemp()['F'], th.GetHumidity(), th.GetPressure()))
+    # oled.Write(0, "T:{}F H:{} P:{}Pa".format((hpt.GetTemp()['F'], th.GetHumidity(), th.GetPressure())))
+    oled.Write(0, "T:{:04.2f} F, H:{:04.2f} %".format(hpt.GetTemp()['F'], th.GetHumidity()))
+    oled.Write(1, "P:{:06.2f} Pa".format(th.GetPressure()))
     time.sleep(0.1)
 if __name__ == "__main__":
   main()
--- display.py
+++ display.py
@@ -5,12 +5,16 @@ import adafruit_ssd1306
 
 class Oled():
   def __init__(self, i2c_bus, address):
-    self.height = 32
+    self.height = 64
     self.width = 128
     self.i2c = i2c_bus
     self.oled = adafruit_ssd1306.SSD1306_I2C(self.width, self.height, self.i2c, addr=address)
     self.font = ImageFont.load_default()
-    self.text = ["", "", ""]
+    font_height = self.font.getsize("TEST")[-1]
+    # Create text storage for number of lines that fit in oled size
+    self.text = []
+    for i in range(round(self.oled.height/(font_height))):
+      self.text.insert(i, "")
 
   def Clear(self):
     self.oled.fill(0)
@@ -20,13 +24,18 @@ class Oled():
   def Write(self, line_num, text):
     image = Image.new("1", (self.oled.width, self.oled.height))
     draw = ImageDraw.Draw(image)
-    font_height = self.font.getsize(text)[-1]
-    draw.text( (0, font_height * line_num), text, font=self.font, fill=255)
-    if len(text) >= 20:
-      print("String too long: %s"%(text))
-    elif (line_num > 2) or (line_num < 0):
-      print("Line num must be > 0 and < 3 it is: %d"%(line_num))
+    (font_width, font_height) = self.font.getsize(text)
+    char_size = font_width // len(text)
+    chars_avail = self.oled.width // char_size
+    if font_width > self.oled.width:
+      print("String too long: %s, %d > %d"%(text, len(text), chars_avail))
+    elif line_num > len(self.text) - 1:
+      print("Too many lines, %d > %d"%(line_num, len(self.text) - 1))
     else:
+      i = 0
+      for line in self.text:
+        draw.text((0, (font_height/2) * i), line, font=self.font, fill=255)
+        i+=2
       self.text[line_num] = text
       self.oled.image(image)
       self.oled.show()
