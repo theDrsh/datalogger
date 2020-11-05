715f2733ef06c9d6c55e18d6e923dd288368351c
drush <rush.daniel95@gmail.com>
2020-11-05 04:14:02 +0000

    Make a aqi function that does rough conversion from pm25 to aqi, not super sure about this sensor.

--- air_quality.py
+++ air_quality.py
@@ -7,5 +7,35 @@ class AirQuality():
     self.i2c = i2c_bus
     self.pm25 = adafruit_pm25.PM25_I2C(self.i2c, None)
   def GetData(self):
-    self.aqdata = self.pm25.read()
+    try:
+      self.aqdata = self.pm25.read()
+      return self.aqdata
+    except RuntimeError:
+      print("Missed a sample")
+      return self.aqdata
     return self.aqdata
+  def GetAQI(self):
+    self.GetData()
+    pm2_5 = self.aqdata["particles 25um"]
+    if pm2_5 <= 55:
+        aqi = round(6.49*(pm2_5**0.778))
+    elif 55 < pm2_5 <= 150:
+        aqi = round(0.518*pm2_5+122)
+    elif 150 < pm2_5:
+        aqi = pm2_5+50
+
+    if aqi <= 50:
+        level = 'GOOD'
+    elif 50 < aqi <= 100:
+        level = 'MODERATE'
+    elif 100 < aqi <= 150:
+        level = 'UNHEALTHY4SOME'
+    elif 150 < aqi <= 200:
+        level = 'UNHEALTHY'
+    elif 200 < aqi <= 300:
+        level = 'VERY UNHEALTHY'
+    elif 300 < aqi < 999:
+        level = 'HAZARDOUS'
+    else:
+        level = 'SENSOR FAULT'
+    return {"AQI" : aqi, "LEVEL" : level}
--- datalogger.py
+++ datalogger.py
@@ -8,7 +8,7 @@ import busio
 
 def main():
   # Initialize i2c bus
-  i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
+  i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
   # Initialize i2c devices, on bus
   oled = display.Oled(i2c, 0x3D)
   aq = air_quality.AirQuality(i2c)
@@ -16,9 +16,14 @@ def main():
   th = temp_humid.TempHumid(i2c)
 
   while True:
-    # oled.Write(0, "T:{}F H:{} P:{}Pa".format((hpt.GetTemp()['F'], th.GetHumidity(), th.GetPressure())))
     oled.Write(0, "T:{:04.2f} F, H:{:04.2f} %".format(hpt.GetTemp()['F'], th.GetHumidity()))
     oled.Write(1, "P:{:06.2f} Pa".format(th.GetPressure()))
-    time.sleep(0.1)
+    aqi = aq.GetAQI()
+    oled.Write(2, "AQI:{}".format(aqi["AQI"]))
+    oled.Write(3, "LEVEL:{}".format(aqi["LEVEL"]))
+    # We already sampled this so we can just grab it from the class
+    data = aq.aqdata
+    oled.Write(4, "25:{} 10:{} 100:{}".format(data['particles 25um'], data['particles 50um'], data['particles 100um']))
+    time.sleep(1)
 if __name__ == "__main__":
   main()
